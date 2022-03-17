d29be8061edc9f49eea820758abebbb0e7a97d9d
Sri <srinivasan.maha@gmail.com>
2022-03-17 00:46:09 -0500

    Cors issue fix and added firebase authendication

--- server/config/firebase-config.js
+++ server/config/firebase-config.js
@@ -1,6 +1,8 @@
+// import { networOptions } from "../config/privateKey";
 const admin = require("firebase-admin");
 
 const serviceAccount = require("./service-account.json");
+// const serviceAccount = require("../config/privateKey");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
